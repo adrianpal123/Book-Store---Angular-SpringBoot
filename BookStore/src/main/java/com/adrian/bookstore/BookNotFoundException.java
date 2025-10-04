534ed9b0c712ecda171a7516cefcbf0335ff7bb9
adrianpal123 <adrianpal26@yahoo.com>
2025-10-04 21:23:34 +0300

    Remove old BookStore folder (replaced by backend/frontend)

--- BookStore/src/main/java/com/adrian/bookstore/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.adrian.bookstore;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("Book not found: " + id);
-    }
-}
--- BookStore/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO books (id, title, author, isbn, price, stock) VALUES
-(1, 'Effective Java', 'Joshua Bloch', '9780134685991', 199.99, 10),
-(2, 'Clean Code', 'Robert C. Martin', '9780132350884', 149.99, 8);
